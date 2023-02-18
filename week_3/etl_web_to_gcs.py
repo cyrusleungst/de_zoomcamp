5b174725bc99d4cb9549e820354c87fe0d2e94d7
Cyrus <cyrus@Cyruss-Mac-mini.local>
2023-02-18 18:39:46 +0000

    added make folder logic

--- week_3/etl_web_to_gcs.py
+++ week_3/etl_web_to_gcs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import os
 import yaml
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
@@ -26,10 +27,16 @@ def fix_dtypes(df: pd.DataFrame, schema: dict) -> pd.DataFrame:
     return df.astype(schema)
 
 
-@task()
+@task(log_prints=True)
 def write_local(df: pd.DataFrame, dataset_file: str, colour: str) -> Path:
     """Write DataFrame out locally as gzipped csv file"""
-    path = Path(f"/Users/cyrus/development/de_zoomcamp/week_3/data/{colour}/{dataset_file}.parquet")
+    parent_dir = Path(f"/Users/cyrus/development/de_zoomcamp/week_3/data/{colour}")
+    path = Path(f"{parent_dir}/{dataset_file}.parquet")
+
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+        print(f"Creating {parent_dir}")
+
     df.to_parquet(path, engine="pyarrow")
     
     return path
@@ -57,11 +64,11 @@ def etl_web_to_gcs(year: int, month: int, colour: str, schemas: dict) -> None:
 
     df = fetch(dataset_url)
     df_clean = fix_dtypes(df, schemas[colour])
-    path = write_local(df_clean, dataset_file)
+    path = write_local(df_clean, dataset_file, colour)
     gcs_path = Path(f"data/{colour}/{dataset_file}.parquet")
     write_gcs(path, gcs_path)
 
-@flow(name="FHV data to GCS")
+@flow(name="NY Taxi data to GCS")
 def etl_parent_flow(months: list[str] = [1, 2], years: list[int] = [2021, 2022], colour: str = "yellow") -> None:
     schemas = load_schemas()
     for year in years:
@@ -69,7 +76,7 @@ def etl_parent_flow(months: list[str] = [1, 2], years: list[int] = [2021, 2022],
             etl_web_to_gcs(year, month, colour, schemas)
 
 if __name__ == '__main__':
-    months=list(range(1, 13))
-    years=[2019, 2020]
-    colour = "yellow"
+    months=list(range(9, 13))
+    years=[2020]
+    colour = "green"
     etl_parent_flow(months, years, colour)
